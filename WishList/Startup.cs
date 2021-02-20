415e54e977a41232552e817d8d71ded4a81e8b76
JaPri <jakraken.pri@gmail.com>
2021-02-20 11:47:20 -0700

    Update Startup.cs

--- WishList/Startup.cs
+++ WishList/Startup.cs
@@ -32,7 +32,7 @@ namespace WishList
             {
                 app.UseExceptionHandler("/Home/Error");
             }
-            app.UseEndpoints(enpoints => { enpoints.MapDefaultControllerRoute(); });
+            app.UseEndpoints(endpoints => { endpoints.MapDefaultControllerRoute(); });
         }
     }
 }
