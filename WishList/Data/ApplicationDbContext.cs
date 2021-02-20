c9dada03a0e00e7447cff8ce000825a86949c7f8
JaPri <jakraken.pri@gmail.com>
2021-02-20 12:29:11 -0700

    DbSetterFix2

--- WishList/Data/ApplicationDbContext.cs
+++ WishList/Data/ApplicationDbContext.cs
@@ -3,9 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
-using WishList.WishList.Modules;
+using WishList.Modules;
 
-namespace WishList.WishList.Data
+namespace WishList.Data
 {
     public class ApplicationDbContext : DbContext
     {
--- WishList/Modules/Item.cs
+++ WishList/Modules/Item.cs
@@ -4,7 +4,7 @@ using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace WishList.WishList.Modules
+namespace WishList.Modules
 {
     public class Item
     {
